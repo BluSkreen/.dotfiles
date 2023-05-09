daec907fa9811133a51d42174f24fb0bd23557a4
BluSkreen <jasonduran303@gmail.com>
2023-05-09 16:50:10 -0600

    update nvim

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,16 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+--    color = color or "catppuccin-macchiato"
+--    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    color = color or "tokyonight"
+
+    require("tokyonight").setup({
+        style = "storm"
+    })
+    vim.cmd.colorscheme(color)
 
 end
 
--- nvim/lua/blueskreen/set.lua
+++ nvim/lua/blueskreen/set.lua
@@ -9,11 +9,11 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
